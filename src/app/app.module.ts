ecec9e7898cb621f1619057be1bd6a0fd01c4d55
JJROMA <jj_roma@yahoo.com.ar>
2022-02-14 12:43:00 -0300

    002

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ItemBarraComponent } from './components/item-barra/item-barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SkillsComponent,
+    ItemBarraComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/item-barra/item-barra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-barra',
+  templateUrl: './item-barra.component.html',
+  styleUrls: ['./item-barra.component.css']
+})
+export class ItemBarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
