5841171fe0b0980dedd0621a1b562d064151bf82
JJRoma <jj_roma@yahoo.ccom.ar>
2022-02-16 21:18:34 -0300

    003

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ItemBarraComponent } from './components/item-barra/item-barra.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,24 @@ import { ItemBarraComponent } from './components/item-barra/item-barra.component
     HeaderComponent,
     AboutComponent,
     SkillsComponent,
-    ItemBarraComponent
+    ItemBarraComponent,
+    ProgressComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgCircleProgressModule.forRoot({
+      "backgroundGradient": true,
+      "backgroundColor": "#ffffff",
+      "backgroundGradientStopColor": "#c0c0c0",
+      "backgroundPadding": -10,
+      "radius": 60,
+      "maxPercent": 100,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#61A9DC",
+      "innerStrokeWidth": 0,
+      "subtitleColor": "#444444",
+      "showInnerStroke": false,
+      "startFromZero": false})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/progress/progress.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
+@Component({
+  selector: 'app-progress',
+  templateUrl: './progress.component.html',
+  styleUrls: ['./progress.component.css']
+})
+export class ProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
